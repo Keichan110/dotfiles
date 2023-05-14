f5e0d6d62ee5b12367e6525c4b9fd152eca4bf2e
ittooco <ittooco@Kekaomakalii.local>
2023-05-15 07:34:41 +0900

    add alacritty & tmux

--- .bin/link.sh
+++ .bin/link.sh
@@ -10,3 +10,26 @@ for dotfile in "${SCRIPT_DIR}"/.??* ; do
 
     ln -fnsv "$dotfile" "$HOME"
 done
+
+# --------------------------------------------------
+# .config以下のファイル
+# --------------------------------------------------
+# shelldon
+TARGET_DIR=".config/sheldon"
+mkdir -p "${HOME}/${TARGET_DIR}"
+ln -fnsv "${SCRIPT_DIR}/${TARGET_DIR}/plugins.toml" "${HOME}/${TARGET_DIR}"
+# tmux
+TARGET_DIR=".config/tmux"
+mkdir -p "${HOME}/${TARGET_DIR}"
+ln -fnsv "${SCRIPT_DIR}/${TARGET_DIR}/default.session.conf" "${HOME}/${TARGET_DIR}"
+
+
+# --------------------------------------------------
+# github関連
+# --------------------------------------------------
+# alacritty-theme
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
+# tmux-plugin-manager
+mkdir -p ~/.config/tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm

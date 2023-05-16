8ca68961b6388c49e93fb8e059eb9f102edb857c
ittooco <ittooco@Kekaomakalii.local>
2023-05-17 08:17:46 +0900

    fix asdf

--- .bin/asdf.sh
+++ .bin/asdf.sh
@@ -2,4 +2,4 @@
 
 #install programming language
 which asdf >/dev/null 2>&1 && asdf plugin add ruby && asdf install ruby latest && asdf global ruby latest
-#which asdf >/dev/null 2>&1 && asdf plugin add nodejs && asdf install nodejs latest && asdf global nodejs latest
+which asdf >/dev/null 2>&1 && asdf plugin add nodejs && asdf install nodejs latest && asdf global nodejs latest

2f552842ed89c4b591accb87e759bca8e2f654f4
ittooco <ittooco@Kekaomakalii.local>
2023-11-23 09:29:57 +0900

    Vim設定を追加

--- .bin/.zshrc
+++ .bin/.zshrc
@@ -14,7 +14,7 @@ path=(
 . /opt/homebrew/opt/asdf/libexec/asdf.sh
 
 # editor
-export EDITOR='nano'
+export EDITOR='vim'
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
--- .bin/defaults.sh
+++ .bin/defaults.sh
@@ -148,13 +148,25 @@ defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 # ====================
 
 # スクリーンショットの保存場所
-defaults write com.apple.screencapture location ~/Pictures
+defaults write com.apple.screencapture location ~/Downloads
 
 # スクリーンショットのファイル名
 defaults write com.apple.screencapture name "Screenshot"
 
 # スクリーンショットの影を消す
 defaults write com.apple.screencapture disable-shadow -bool true
+
+# ====================
+#
+# VSCode
+#
+# ====================
+# キーリピートを有効化
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false              # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false      # For VS Code Insider
+defaults write com.vscodium ApplePressAndHoldEnabled -bool false                      # For VS Codium
+defaults write com.microsoft.VSCodeExploration ApplePressAndHoldEnabled -bool false   # For VS Codium Exploration users
+defaults delete -g ApplePressAndHoldEnabled                                           # If necessary, reset global default
 
 # ====================
 #
